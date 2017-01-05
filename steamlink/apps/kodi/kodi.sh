a0edad94862ccddc90b10efdb0551c0314a7b134
Parker Reed <parker.l.reed@gmail.com>
2017-01-05 02:23:24 -0500

    Current packages

--- steamlink/apps/kodi/kodi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Start Kodi with the correct environment on the Steam Link
-
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/lib/pulseaudio:${PWD}/lib"
-export KODI_HOME="${PWD}/share/kodi"
-export KODI_HOME_BIN="${PWD}/lib/kodi"
-export PYTHONHOME="${PWD}"
-exec ./bin/kodi
